6255b95a6e49f0a07fba0a0bb2812a6f8d69f67a
JISMI5SAJU <146074614+JISMI5SAJU@users.noreply.github.com>
2024-06-27 20:39:31 +0530

    Update FormatIndex.py

--- FormatIndex.py
+++ FormatIndex.py
@@ -3,3 +3,6 @@ item=567
 price=30000
 myorder="i want to pay{2}dollars for {0} pieces of item{1}"
 print(myorder.format(quantity,item,price))
+
+OUTPUT:
+i want to pay30000dollars for 3 pieces of item567
